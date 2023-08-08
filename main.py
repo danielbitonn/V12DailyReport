e66436fc74c244d70b831a86154659b82a7b9088
Daniel Biton <danielbitonn@gmail.com>
2023-08-09 01:34:57 +0300

    # V112 2023-08-08 ## APPLOGGER ## CONF & META DATA
    # Next
    ## CHOOSE MOO & DATE & TIME
    ## PARALLEL _ CLOUD SYNC

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# TODO: f
+# TODO:
 #  ################################################## Program Design ###################################################
 #          REQUIREMENTS:
 #                       IMPORTS
@@ -8,10 +8,26 @@
 #                       BKG # AZURE INTERFACE
 #                       DATE & TIME PICKER [time-dimmed]
 #
+
+from src.scripts.system.config import DMDD
+from src.scripts.system.applogger import APPLOGGER
 from src.scripts.gui.gui_manager import main_gui_start
 
 if __name__ == '__main__':
+    # METADATA thread
+    x=3
+    print(f'fdsfsdf:{DMDD}')
+    APPLOGGER.info(f'DONE # <read_or_write_meta_data_json()> has been completed <{x}>')
+
+    # Main Gui Thread
     main_gui_start()
-    print("bitinn")
-    print("danielBitonn")
+    # tr_main_gui_start = threading.Thread(target=main_gui_start(), daemon=True)
+    # tr_main_gui_start.start()
+
+
+    # Wait for the dtr_meta_data to be set
+    # META_DATA = qtr_meta_data.get()
+    # print(META_DATA)
+
+    print("THE END")
 
