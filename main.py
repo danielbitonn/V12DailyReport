61fbd3101b218c529ba234cf922fb77f7bbc8163
Daniel Biton <danielbitonn@gmail.com>
2023-08-08 12:25:48 +0300

    # Structure

---
+++ main.py
@@ -0,0 +1,15 @@
+# TODO: f
+#  ################################################## Program Design ###################################################
+#          REQUIREMENTS:
+#                       IMPORTS
+#          SYSTEM:
+#                       GUI MANAGER
+#                       APP LOGGER
+#                       BKG # AZURE INTERFACE
+#                       DATE & TIME PICKER [time-dimmed]
+#
+from src.scripts.gui.gui_manager import main_gui_start
+
+if __name__ == '__main__':
+    main_gui_start()
+    
