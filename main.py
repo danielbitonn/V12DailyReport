d3b487e8cdbd6170a13a79b3ba1fc819c79c7c87
Daniel Biton <danielbitonn@gmail.com>
2023-08-10 22:33:09 +0300

    # V112 2023-08-10 ## web_gui by flask (web_gui.py)
    # Next
    ## CHOOSE MOO & DATE & TIME
    ## PARALLEL _ CLOUD SYNC

--- main.py
+++ main.py
@@ -4,30 +4,74 @@
 #                       IMPORTS
 #          SYSTEM:
 #                       GUI MANAGER
-#                       APP LOGGER
+#                       app LOGGER
 #                       BKG # AZURE INTERFACE
 #                       DATE & TIME PICKER [time-dimmed]
-#
+#  #####################################################################################################################
 
+import threading
+import time
 from src.scripts.system.config import DMDD
 from src.scripts.system.applogger import APPLOGGER
 from src.scripts.gui.gui_manager import main_gui_start
+from src.scripts.gui.web_gui import start_web_server, get_default_browser_windows,terminate_process_by_port, should_shutdown
+import requests
+
+
+def is_browser_open(driver):
+    try:
+        driver.current_window_handle
+        return True
+    except:
+        return False
+
+def monitor_browser(driver, should_shutdown):
+    while True:
+        time.sleep(1)
+        if not is_browser_open(driver):
+            print("Browser window was closed by the user.")
+            should_shutdown = True
+            requests.post(f'http://127.0.0.1:{PORT}/shutdown_check')  # Shut down the server
+            if driver:
+                driver.quit()  # Close the browser
+            break
+
+
+PORT = 6005
 
 if __name__ == '__main__':
-    # METADATA thread
-    x=3
+
+    try:
+        result = terminate_process_by_port(PORT)
+        print(result)
+    except Exception as e:
+        print(e)
+
+    # # TODO: web_gui execution
+    web_server_thread = threading.Thread(target=start_web_server, args=(PORT,), daemon=True)
+    web_server_thread.start()
+
+    # TODO: open browser
+    driver = get_default_browser_windows()
+    driver.get(f"http://127.0.0.1:{PORT}")
+
+    # TODO: METADATA thread
     print(f'fdsfsdf:{DMDD}')
-    APPLOGGER.info(f'DONE # <read_or_write_meta_data_json()> has been completed <{x}>')
-
-    # Main Gui Thread
-    main_gui_start()
-    # tr_main_gui_start = threading.Thread(target=main_gui_start(), daemon=True)
-    # tr_main_gui_start.start()
-
+    APPLOGGER.info(f'DONE # <read_or_write_meta_data_json()> has been completed')
 
     # Wait for the dtr_meta_data to be set
     # META_DATA = qtr_meta_data.get()
     # print(META_DATA)
 
-    print("THE END")
 
+    print("THE")
+    monitor_thread = threading.Thread(target=monitor_browser, args=(driver, should_shutdown), daemon=True)
+    monitor_thread.start()
+
+    # TODO: Main Gui Thread
+    main_gui_start()
+
+    requests.post(f'http://127.0.0.1:{PORT}/shutdown_check')  # Shut down the server
+    if driver:
+        driver.quit()  # Close the browser
+    print("END")
